6bb1c8afa6e0401276918644c65ad504e29ac27d
soul071213 <127070775+soul071213@users.noreply.github.com>
2024-06-13 01:50:15 +0000

    context

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
 import "./styles.css";
 import { Routes, Route } from "react-router-dom";
-
+import React, { useState, useCallback } from "react";
 import Header from "./components/Header";
 import Card from "./components/Card";
+// import NoPage from "./components/Nopage.jsx";
 import CardList from "./CardList.jsx";
 // import Home from "./components/Home";
 import HomeAxios from "./HomeAxios.jsx";
 import DetailTab from "./DetailTab.jsx";
 // import AxiosTest from "./AxiosTest.jsx";
-// import NoPage from "./component";
 
+export let Context1 = React.createContext();
 
 
 export default function App() {
+  // const [users, setUsers] = useState(["user1", "user2", "user3"]);
+  const [theme, setTheme] = useState("light");
+
+  const toggleTheme = useCallback(() => {
+    setTheme(theme === "light" ? "dark" : "light");
+  }, [theme]);
+
   return (
     <div className="App">
       <Routes>
@@ -20,8 +28,14 @@ export default function App() {
         <Route path="/header" element={<Header />} />
         <Route path="/card" element={<Card />} />
         <Route path="/cardList" element={<CardList />} />
-        <Route path="/detail/:id" element={<DetailTab />} />
-        { /*  <Route path="*" element={<NoPage />} /> */}
+
+        <Route path="/detail/:id" element={
+          <Context1.Provider value={{theme, toggleTheme}} >
+            <DetailTab />
+           </Context1.Provider>
+          } />
+
+        {/*} <Route path="*" element={<NoPage />} /> */}
       </Routes>
     </div>
   );
--- src/DetailTab.jsx
+++ src/DetailTab.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react";
-import { Nav } from "react-bootstrap";
+import { useState, useContext } from "react";
+import { Nav, Button } from "react-bootstrap";
 import "./styles.css";
 import { useParams } from "react-router-dom";
-import Rooms from "./Rooms.js";
+import Rooms from "./Rooms";
+import { Context1 } from "./App.jsx";
 
 export default function DetailTab() {
+  const { theme, toggleTheme } = useContext(Context1);
   let { id } = useParams();
   console.log(id);
 
@@ -16,63 +18,74 @@ export default function DetailTab() {
   console.log(result.name);
 
   return (
-    <div className="container">
-      <div className="box image">
-        <img src={result.image} className="boximage" alt="Room" />
-      </div>
-      <div className="box">
-        <div className="location">{result.name} </div>
-        <div className="location">{result.location}에 위치</div>
-        <div className="title">{result.title}</div>
-        <hr />
-        <div className="guest">최대인원{result.totalGuest} 명</div>
-        <div className="rating">
-          평점: {result.rating}{" "}
-          <span className="visits">({result.numberOfRating})</span>
+    <main
+      style={{
+        backgroundColor: theme === "light" ? "white" : "black",
+        color: theme === "light" ? "black" : "white",
+      }}
+    >
+      <div>테마색깔 : {theme}</div>
+      <Button onClick={toggleTheme} id="theme" variant="light">
+        테마변경
+      </Button>
+      <div className="container">
+        <div className="box image">
+          <img src={result.image} className="boximage" alt="Room" />
         </div>
-        <div className="price">
-          {result.price} 원<span className="month">/ month</span>
-        </div>
-        <Nav variant="tabs" defaultActiveKey="link0">
-          <Nav.Item>
-            <Nav.Link eventKey="link0" onClick={() => setTab(0)}>
-              상세정보
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link1" onClick={() => setTab(1)}>
-              리뷰
-            </Nav.Link>
-          </Nav.Item>
-          <Nav.Item>
-            <Nav.Link eventKey="link2" disabled>
-              상품Q&A
-            </Nav.Link>
-          </Nav.Item>
-        </Nav>
+        <div className="box">
+          <div className="location">{result.name} </div>
+          <div className="location">{result.location}에 위치</div>
+          <div className="title">{result.title}</div>
+          <hr />
+          <div className="guest">최대인원 {result.totalGuest} 명</div>
+          <div className="rating">
+            평점: {result.rating}{" "}
+            <span className="visits">({result.numberOfRating})</span>
+          </div>
+          <div className="price">
+            {result.price} 원<span className="month">/ month</span>
+          </div>
+          <Nav variant="tabs" defaultActiveKey="link0">
+            <Nav.Item>
+              <Nav.Link eventKey="link0" onClick={() => setTab(0)}>
+                상세정보
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link1" onClick={() => setTab(1)}>
+                리뷰
+              </Nav.Link>
+            </Nav.Item>
+            <Nav.Item>
+              <Nav.Link eventKey="link2" disabled>
+                상품Q&A
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
 
-        <div>
-          {tab === 0 && (
-            <div>
-              <p>
-                5~9층에 위치한 스탠다드룸은 편안함과 쾌적함이 인상적이 휴식
-                환경을 제공합니다.
-              </p>
-              <p>
-                모던한 아름다움과 신라호텔 수준의 고급 침구 및 침대 그리고 고급
-                어메니티로 한 단계 업그레이드 된 휴식을 경험하십시오.
-              </p>
-              <br /> 위치 :5~9층
-              <br />
-              룸구성 : 침실 1, 욕실 1
-              <br />
-              문의전화 : 02-2230-0700
-            </div>
-          )}
-          {tab === 1 && <div>내용1</div>}
-          {tab === 2 && <div>내용2</div>}
+          <div>
+            {tab === 0 && (
+              <div>
+                <p>
+                  5~9층에 위치한 스탠다드룸은 편안함과 쾌적함이 인상적이 휴식
+                  환경을 제공합니다.
+                </p>
+                <p>
+                  모던한 아름다움과 신라호텔 수준의 고급 침구 및 침대 그리고
+                  고급 어메니티로 한 단계 업그레이드 된 휴식을 경험하십시오.
+                </p>
+                <br /> 위치 : 5~9층
+                <br />
+                룸구성 : 침실 1, 욕실 1
+                <br />
+                문의전화 : 02-2230-0700
+              </div>
+            )}
+            {tab === 1 && <div>내용1</div>}
+            {tab === 2 && <div>내용2</div>}
+          </div>
         </div>
       </div>
-    </div>
+    </main>
   );
 }
