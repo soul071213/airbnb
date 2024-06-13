f65c649d4ebc50cfc3d564b33c3dc95c117a6edd
soul071213 <127070775+soul071213@users.noreply.github.com>
2024-06-13 00:44:51 +0000

    detailTab

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { Routes, Route } from "react-router-dom";
 
 import Header from "./components/Header";
 import Card from "./components/Card";
-import NoPage from "./components/NoPage.jsx";
-import CardList from "./CardList";
-import Home from "./components/Home";
+import CardList from "./CardList.jsx";
+// import Home from "./components/Home";
 import HomeAxios from "./HomeAxios.jsx";
-import Detail from "./components/Detail.jsx";
-import AxiosTest from "./AxiosTest.jsx";
+import DetailTab from "./DetailTab.jsx";
+// import AxiosTest from "./AxiosTest.jsx";
+// import NoPage from "./component";
+
+
 
 export default function App() {
   return (
@@ -18,9 +20,9 @@ export default function App() {
         <Route path="/header" element={<Header />} />
         <Route path="/card" element={<Card />} />
         <Route path="/cardList" element={<CardList />} />
-        <Route path="/detail/:id" element={<Detail />} />
-        <Route path="*" element={<NoPage />} />
-      </Routes>{" "}
+        <Route path="/detail/:id" element={<DetailTab />} />
+        { /*  <Route path="*" element={<NoPage />} /> */}
+      </Routes>
     </div>
   );
 }
